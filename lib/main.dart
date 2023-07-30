77824a6e24dea21ce822606f3fe27b720008c87f
Yasin <mohammadyasin9064@gmail.com>
2023-07-30 21:34:21 +0530

    Learn about Container

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: Text('Flutter Container'),
+      ),
+      body:
+      Center(
+
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.cyan,
+          child: Center(
+            child: Text('Hello Paradox!'),
+          )
+        ),
+      )
+        
+    );
+  }
+}
