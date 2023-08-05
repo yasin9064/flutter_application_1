658fe1f04ccab00c4863be7be744ee9e8a6fe215
Yasin <mohammadyasin9064@gmail.com>
2023-08-05 20:44:19 +0530

    Learn about ScrollView.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -9,32 +9,87 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text("InkWell Widget"),
         ),
-        body: Center(
-          child: InkWell(
-            onTap: () {
-              print('Got Tapped!');
-            },
-            onDoubleTap: () {
-              print('Got Double-Tapped!');
-            },
-            onLongPress: () {
-              print('Got LongPressed!');
-            },
-            child: Container(
-              width: 200,
-              height: 75,
-              color: Colors.green,
-              child: Center(
-                  child: InkWell(
-                      onTap: () {
-                        print('Text got Tapped!');
-                      },
-                      child: Text(
-                        'Click Here!',
-                        style: TextStyle(fontSize: 25),
-                      ))),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 11),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 11),                  
+                          height: 200,
+                          width: 200,
+                          color: Colors.lightBlue,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 11),                  
+                          height: 200,
+                          width: 200,
+                          color: Colors.grey,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 11),                  
+                          height: 200,
+                          width: 200,
+                          color: Colors.pink,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 11),                  
+                          height: 200,
+                          width: 200,
+                          color: Colors.green,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 11),                  
+                          height: 200,
+                          width: 200,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),                 
+                  height: 200,
+                  color: Colors.purple,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),                 
+                  height: 200,
+                  color: Colors.orange,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),                 
+                  height: 200,
+                  color: Colors.lightGreen,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),                  
+                  height: 200,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  color: Colors.red,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 11),
+                  height: 200,
+                  color: Colors.black,
+                ),
+              ],
             ),
           ),
-        ));
+        )
+        
+        
+        );
   }
 }
