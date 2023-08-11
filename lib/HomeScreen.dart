52e0bfcfb5392a4ff27d5c5b5cfb82f22c9b8f2f
Yasin <mohammadyasin9064@gmail.com>
2023-08-11 21:39:00 +0530

    Learn about Expanded Widget.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -10,34 +10,41 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
         appBar: AppBar(
-          title: Text("List View"),
+          title: Text("Expanded Widget"),
         ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-           child: Center(
-             child: Container(
-             width: 150,
-             height: 150,
-             decoration: BoxDecoration(
-              color: Colors.grey,
-              // borderRadius: BorderRadius.circular(24),
-              border: Border.all(
-                width: 3,
-                color: Colors.red,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 21,
-                  color: Colors.cyan,
-                  spreadRadius: 21
-                )
-              ],
-              shape: BoxShape.circle
-        ),
-       ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                height: 100,
+                color: Colors.black,
+              )
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                height: 100,
+                color: Colors.blue,
+              )
+            ),
+            Expanded(
+              flex:2,
+              child: Container(
+                height: 100,
+                color: Colors.brown,
+              )
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                height: 100,
+                color: Colors.orange
+              )
+            ),
+
+          ],
       )
-     )
     );
   }
 }
