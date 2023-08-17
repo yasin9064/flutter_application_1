84ed1e6f5321efe1337daef3f90720eb0f52e53e
Yasin <mohammadyasin9064@gmail.com>
2023-08-17 22:17:01 +0530

    Learn about List Tile.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -5,23 +5,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-
-
+    var arrNames = [
+      'Asim',
+      'Yasin',
+      'Artaza',
+      'Zishan',
+      'Amar',
+      'Tarique',
+      'Meraj',
+      'Raunaque',
+      'Maher'
+    ];
+    // var arrNumbers = ['1, 2, 3, 4, 5, 6, 7, 8, 9'];
     return Scaffold(
         appBar: AppBar(
-          title: Text("Expanded Widget"),
+          title: Text("List Tile"),
         ),
-        body: Container(
-          color: Colors.cyan,
-          margin: EdgeInsets.only(top: 30, left: 40),
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text('Paradox!',style: TextStyle(fontSize: 30,color: Colors.black),),
-          ),
-        )
-    );
+        body: ListView.separated(
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Text('${index+1}'),
+                title: Text(arrNames[index]),
+                subtitle: Text('Number'),
+                trailing: Icon(Icons.add),
+              );
+            },
+            itemCount: arrNames.length,
+            separatorBuilder: (context, index) {
+              return Divider(
+                height: 20,
+                thickness: 1,
+              );
+            }));
   }
 }
-
-
