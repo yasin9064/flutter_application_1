1dde87bb11df44c540a87dab3a263225d8e25e29
Yasin <mohammadyasin9064@gmail.com>
2023-08-22 21:45:58 +0530

    Get Current Date & Time.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,91 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/UI_Helper/utii.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    var emailText = TextEditingController();
-    var passText = TextEditingController();
+    var time = DateTime.now();
     return Scaffold(
         appBar: AppBar(
-          title: Text("Input Field"),
+          title: Text("Date & Time"),
         ),
         body: Center(
-            child: Container(
-          width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: emailText,
-                keyboardType: TextInputType.emailAddress,      
-                decoration: InputDecoration(
-                  hintText: 'Enter Email',
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide(
-                          color: Colors.deepOrange,
-                          width: 2.5,
-                        )),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 1.5,
-                        )),
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Colors.blueGrey,
-                    )),
-              ),
-              Container(
-                height: 11,
-              ),
-              TextField(
-                controller: passText,
-                obscureText: true,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  hintText: 'Enter Password',
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(
-                        color: Colors.deepOrange,
-                        width: 2.5,
-                      )),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(
-                        color: Colors.blue,
-                        width: 1.5,
-                      )),
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.blueGrey,
-                  ),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.remove_red_eye, color: Colors.blueGrey),
-                    onPressed: () {
-                      
-                    },
-                  ),
+          child: Container(
+            width: 200,
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'CurrentTime: ${time.hour}:${time.minute}:${time.second}',
+                  style: TextStyle(fontSize: 25),
                 ),
-              ),
-              Container(height: 8),
-              ElevatedButton(
+                ElevatedButton(
                   onPressed: () {
-                    String userEmail = emailText.text.toString();
-                    String userPassword = passText.text;
-
-                    print('Email: $userEmail, Password: userPassword');
+                    setState(() {});
                   },
-                  child: Text(
-                    'Login',
-                  ))
-            ],
+                  child: Text('CurrentTime'),
+                )
+              ],
+            ),
           ),
-        )));
+        ));
   }
 }
