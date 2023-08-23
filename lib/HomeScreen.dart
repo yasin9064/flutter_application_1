f759e51dc5c2860b5bfcbf411ee33b7d4278f3bc
Yasin <mohammadyasin9064@gmail.com>
2023-08-24 00:08:52 +0530

    Learn about DateTime Picker.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/UI_Helper/utii.dart';
 import 'package:intl/intl.dart';
@@ -12,31 +14,46 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    var time = DateTime.now();
+    var datePicked = DateTime.now();
+    var timePicked = DateTime.now();
     return Scaffold(
         appBar: AppBar(
           title: Text("Date & Time"),
         ),
         body: Center(
-          child: Container(
-            width: 200,
-            height: 200,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'CurrentTime: ${DateFormat('yMMMMEEEEd').format(time)}',
-                  style: TextStyle(fontSize: 25),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {});
-                  },
-                  child: Text('CurrentTime'),
-                )
-              ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Select Date',
+              style: TextStyle(fontSize: 25),
             ),
-          ),
-        ));
+            ElevatedButton(
+                onPressed: () async {
+                  DateTime? datePicked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2022),
+                      lastDate: DateTime(2024));
+                  if (datePicked != null) {
+                    print(
+                        'Date Selected: ${DateFormat('yMMMMd').format(datePicked)}');
+                  }
+                },
+                child: Text('Show')),
+            ElevatedButton(
+                onPressed: () async {
+                  TimeOfDay? timePicked = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.now(),
+                      initialEntryMode: TimePickerEntryMode.dial);
+                  if (timePicked != null) {
+                    print('Time Selected: ${timePicked.hour}:${timePicked.minute}');
+                        
+                  }
+                },
+                child: Text('Select Time'))
+          ],
+        )));
   }
 }
