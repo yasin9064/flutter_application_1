24bb1fb2597951f6da0d6ba9b6eb78ab2c50ca81
Yasin <mohammadyasin9064@gmail.com>
2023-08-24 11:50:49 +0530

    Learn about CallBack Function.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -14,64 +14,19 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    var arrColors = [
-      Colors.red,
-      Colors.blue,
-      Colors.yellow,
-      Colors.green,
-      Colors.pink,
-      Colors.black,
-      Colors.purple,
-      Colors.orange,
-      Colors.brown,
-    ];
+    callBack() {
+      print('Clicked!!!!');
+    }
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Grid VIew"),
+          title: const Text("CallBack Function"),
         ),
-        body: GridView.builder(itemBuilder: (context, index) {
-          return Container(
-            color: arrColors[index],
-          );
-        }, itemCount: arrColors.length, gridDelegate:
-         SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 150,
-         crossAxisSpacing: 11,
-         mainAxisSpacing: 11,),
-        )
-
-        // GridView.count(
-        //   crossAxisCount: 3,
-        //   crossAxisSpacing: 11,
-        //   mainAxisSpacing: 11,
-        //   children: [
-        //     Container(color: arrColors[0]),
-        //     Container(color: arrColors[1]),
-        //     Container(color: arrColors[2]),
-        //     Container(color: arrColors[3]),
-        //     Container(color: arrColors[4]),
-        //     Container(color: arrColors[5]),
-        //     Container(color: arrColors[6]),
-        //     Container(color: arrColors[7]),
-        //     Container(color: arrColors[8]),
-        //   ],
-        // ),
-
-        // GridView.extent(maxCrossAxisExtent: 100,
-        // crossAxisSpacing: 11,
-        // mainAxisSpacing: 11,
-        // children: [
-        //     Container(color: arrColors[0]),
-        //     Container(color: arrColors[1]),
-        //     Container(color: arrColors[2]),
-        //     Container(color: arrColors[3]),
-        //     Container(color: arrColors[4]),
-        //     Container(color: arrColors[5]),
-        //     Container(color: arrColors[6]),
-        //     Container(color: arrColors[7]),
-        //     Container(color: arrColors[8]),
-        // ],
-        // )
-
-        );
+        body: Center(
+          child: ElevatedButton(
+            child: Text('Click Here!'),
+            onPressed: callBack,
+          ),
+        ));
   }
 }
