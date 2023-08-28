80e362e7b10ce8567bfe3511cecc8c107e4cb2d0
Yasin <mohammadyasin9064@gmail.com>
2023-08-28 22:49:23 +0530

    Learn about Split App into Widget.

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -14,19 +14,59 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    callBack() {
-      print('Clicked!!!!');
-    }
-
     return Scaffold(
         appBar: AppBar(
-          title: const Text("CallBack Function"),
+          title: const Text("Split App into Widget"),
         ),
-        body: Center(
-          child: ElevatedButton(
-            child: Text('Click Here!'),
-            onPressed: callBack,
-          ),
+        body: Container(
+          child: Column(children: [
+            StatusBar(),
+            Contacts(),
+          ]),
         ));
+  }
+}
+
+class StatusBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.blueGrey,
+        child: ListView.builder(
+          itemBuilder: ((context, index) => const Padding(
+                padding: EdgeInsets.all(11.0),
+                child: SizedBox(
+                  width: 100,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white70,
+                  ),
+                ),
+              )),
+          itemCount: 11,
+          scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
+  }
+}
+
+class Contacts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 4,
+      child: Container(
+        color: Colors.orangeAccent,
+        child: ListView.builder(
+            itemBuilder: ((context, index) => const ListTile(
+                  leading: CircleAvatar(backgroundColor: Colors.blue),
+                  title: Text('Name'),
+                  subtitle: Text('Mob No'),
+                  trailing: Icon(Icons.delete),
+                ))),
+      ),
+    );
   }
 }
