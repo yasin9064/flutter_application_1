e23934487a47f8ea0b457597cd317edf73d447d0
Yasin <mohammadyasin9064@gmail.com>
2023-09-26 19:45:14 +0530

    Learn about Animated Container

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -11,167 +11,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var weightController = TextEditingController();
-
-  var feetController = TextEditingController();
-
-  var inchController = TextEditingController();
-
-  var result = '';
-  var bgColor;
+  var _width = 200.0;
+  var _height = 100.0;
+  Decoration myDecor = BoxDecoration(
+    borderRadius: BorderRadius.circular(5),
+    color: Colors.indigo
+  );
+  var flag = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Center(child: Text('BMI')),
+          title: Center(child: Text('Animated Container')),
         ),
-        body: Container(
-          color: bgColor,
-          child: Center(
-            child: Container(
-              width: 380,
-              child: Column(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'B',
-                      style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.red),
-                    ),
-                    Text(
-                      'M',
-                      style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.amber),
-                    ),
-                    Text(
-                      'I',
-                      style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.green),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 30),
-                TextField(
-                  controller: weightController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      label: Text(
-                        'Enter your Weight(in Kg)',
-                        style: TextStyle(color: Colors.red, fontSize: 18),
-                      ),
-                      prefixIcon: Icon(Icons.line_weight, color: Colors.red),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(11),
-                          borderSide:
-                              BorderSide(color: Colors.red, width: 1.5)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(11),
-                          borderSide: BorderSide(
-                              color: Colors.red.shade300, width: 1.5))),
-                ),
-                SizedBox(height: 15),
-                TextField(
-                  controller: feetController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      label: Text(
-                        'Enter your Height(in Feet)',
-                        style: TextStyle(color: Colors.amber, fontSize: 18),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.height,
-                        color: Colors.amber,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(11),
-                        borderSide: BorderSide(color: Colors.amber, width: 1.5),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(11),
-                          borderSide: BorderSide(
-                              color: Colors.amber.shade300, width: 1.5))),
-                ),
-                SizedBox(height: 15),
-                TextField(
-                  controller: inchController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      label: Text(
-                        'Enter your Height(in Inch)',
-                        style: TextStyle(color: Colors.green, fontSize: 18),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.height,
-                        color: Colors.green,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(11),
-                        borderSide: BorderSide(color: Colors.green, width: 1.5),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(11),
-                          borderSide: BorderSide(
-                              color: Colors.green.shade300, width: 1.5))),
-                ),
-                SizedBox(height: 15),
-                ElevatedButton(
-                    onPressed: () {
-                      var weight = weightController.text.toString();
-                      var feet = feetController.text.toString();
-                      var inch = inchController.text.toString();
-
-                      if (weight != '' && feet != '' && inch != '') {
-                        //BMI Calculation
-                        var iWeight = int.parse(weight);
-                        var iFeet = int.parse(feet);
-                        var iInch = int.parse(inch);
-
-                        var tInch = (iFeet * 12) + iInch;
-                        var tCm = tInch * 2.54;
-                        var tMeter = tCm / 100;
-                        var bmi = iWeight / (tMeter * tMeter);
-
-                        var message = '';
-
-                        if (bmi > 25) {
-                          message = "You're OverWeight!!";
-                          bgColor = Colors.red.shade200;
-                        } else if (bmi < 18) {
-                          message = "You're UnderWeight!!";
-                          bgColor = Colors.amber.shade200;
-                        } else {
-                          message = "You're Healthy!!";
-                          bgColor = Colors.green.shade200;
-                        }
-
-                        setState(() {
-                          result = '$message \n Your BMI is: ${bmi.toStringAsFixed(2)}';
-                        });
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                width: _width,
+                height: _height,
+                decoration: myDecor,
+                curve: Curves.bounceOut,
+                duration: Duration(seconds: 3),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (flag) {
+                        _width = 100.0;
+                        _height = 200.0;
+                        myDecor = BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Colors.yellow
+                        );
+                        flag = false;
                       } else {
-                        setState(() {
-                          result = 'Please fill all the required fields!!';
-                        });
+                        _width = 200.0;
+                        _height = 100.0;
+                        myDecor = BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.indigo
+                        );
+                        flag = true;
                       }
-                    },
-                    child: Text(
-                      'Calculate',
-                      style: TextStyle(fontSize: 22),
-                    )),
-                SizedBox(height: 11),
-                Text(
-                  result,
-                  style: TextStyle(fontSize: 18),
-                )
-              ]),
-            ),
+                    });
+                  },
+                  child: Text('Animate'))
+            ],
           ),
         ));
   }
